8859691bd81cfcb16147960acd5b9d3a95602b96
VadimBaby <vadimdim22@gmail.com>
2025-06-26 20:49:32 +0700

    added support for all platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MyCommon",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v15), .macOS(.v13), .watchOS(.v9), .visionOS(.v1), .tvOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
